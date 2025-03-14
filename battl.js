1281e3147f13a1a47ce30e03c832935522d0be0b
0Benedict0 <vladkorinenko123@gmail.com>
2025-03-14 22:27:36 +0200

    cod fix

--- battl.js
+++ battl.js
@@ -1,32 +1,53 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location1 + 2;
+
 var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while  (isSunk == false){
-    guess = prompt('Ready, aim, fire! ( enter a number from 0-6):');
-    if (guess< 0 || guess > 6) {
-        alert('please enter a valid cell number!');
-    } else {
-        guesses = guesses + 1;
 
-        if (guess == location1 || guess == location2 || guess == location3) {
-            alert('HIT!');
-            hits = hits + 1;
-            if (hits ==3) {
-                isSunk = true;
-                alert('You sank my battleship!');
+var hitLocations = [];
 
-            }
+while (!isSunk) {
+  
+  guess = prompt("Ready, aim, fire! (Enter a number from 0-6):");
 
-        }else {
-            alert('MISS');
-        }
+  
+  if (guess < 0 || guess > 6 || isNaN(guess)) {
+    alert("Please enter a valid cell number!");
+  } else {
+    
+    guesses++;
+
+   
+    if (hitLocations.includes(guess)) {
+      alert("You already hit this spot! Try another.");
+      continue;
     }
 
+  
+    if (guess == location1 || guess == location2 || guess == location3) {
+      alert("HIT!");
+      hits++;
+
+     
+      hitLocations.push(guess);
+
+     
+      if (hits == 3) {
+        isSunk = true;
+        alert("You sank my battleship!");
+      }
+    } else {
+      alert("MISS!");
+    }
+  }
 }
-var stats = ' You took ' + guesses + ' guesses to sink the battleship, ' + ' which meanss your shooting accuracy was ' + (3 / guesses);
+
+
+var accuracy = (3 / guesses) * 100;
+var stats = `You took ${guesses} guesses to sink the battleship. Your shooting accuracy was ${accuracy.toFixed(2)}%.`;
 alert(stats);
